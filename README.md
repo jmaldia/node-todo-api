0428f4e8e0d3f98262eb07a5a66e22a4ebf23eb0
Jon Maldia <jon@maldia.net>
2018-11-21 11:17:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NODE TODO API
+## by Jon Maldia
+A node to do app. 
+
+## Technologies Used
+- Node.js
+- MongoDB
---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    } 
+
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     test: 'Something to do #3',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Christina Maldia',
+    //     age: 39,
+    //     location: 'New York'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
+
+    client.close();
+});
